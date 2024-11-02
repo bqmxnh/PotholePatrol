366eca8d844d82000142a57510c0c2f195acd104
Quoc Minh Bui <22520855@gm.uit.edu.vn>
2024-11-02 22:06:53 +0700

    Add changes

---
+++ app/src/main/java/com/example/potholepatrol/Fragment/FragmentMap.java
@@ -0,0 +1,20 @@
+package com.example.potholepatrol.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.potholepatrol.R;
+
+public class FragmentMap extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_map, container, false);
+    }
+}
