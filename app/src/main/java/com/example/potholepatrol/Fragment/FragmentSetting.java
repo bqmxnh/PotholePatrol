13d137d871ebdc860bc98ede05b717300da2f5df
Quoc Minh Bui <22520855@gm.uit.edu.vn>
2024-11-04 19:14:39 +0700

    Add Setting UI and modified simple operation

--- app/src/main/java/com/example/potholepatrol/Fragment/FragmentSetting.java
+++ app/src/main/java/com/example/potholepatrol/Fragment/FragmentSetting.java
@@ -15,27 +15,33 @@ import com.example.potholepatrol.R;
 
 public class FragmentSetting extends Fragment {
     private int originalStatusBarColor;
+    private int originalSystemUiVisibility;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Lưu màu thanh trạng thái gốc
         Window window = requireActivity().getWindow();
-        originalStatusBarColor = window.getStatusBarColor();
 
-        // Làm cho thanh trạng thái trong suốt
+        // Lưu trạng thái ban đầu của thanh trạng thái và giao diện hệ thống
+        originalStatusBarColor = window.getStatusBarColor();
+        originalSystemUiVisibility = window.getDecorView().getSystemUiVisibility();
+
+        // Làm cho thanh trạng thái trong suốt và đặt chế độ toàn màn hình
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(android.graphics.Color.TRANSPARENT);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         return inflater.inflate(R.layout.activity_setting, container, false);
     }
-    // Ví dụ Setting -> Dashboard thi no se doi lai mau xanh
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        // Khôi phục màu thanh trạng thái ban đầu
+
+        // Khôi phục trạng thái ban đầu của thanh trạng thái và giao diện hệ thống
         Window window = requireActivity().getWindow();
         window.setStatusBarColor(originalStatusBarColor);
+        window.getDecorView().setSystemUiVisibility(originalSystemUiVisibility);
     }
 }
