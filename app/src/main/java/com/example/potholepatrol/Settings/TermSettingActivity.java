91b784756be9057cbba845e6c553a4dbaae58a8b
Quoc Minh Bui <22520855@gm.uit.edu.vn>
2024-12-08 17:49:31 +0700

    Done Terms & Policies

--- app/src/main/java/com/example/potholepatrol/Settings/TermSettingActivity.java
+++ app/src/main/java/com/example/potholepatrol/Settings/TermSettingActivity.java
@@ -1,5 +1,6 @@
 package com.example.potholepatrol.Settings;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,5 +13,8 @@ public class TermSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_terms_policies);
+        // Set up back button
+        ImageView btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(v -> finish());
     }
 }
